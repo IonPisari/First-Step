fd55e630f83a1c778b61f7815e6f0764e8856a02
IonPisari <92115769+IonPisari@users.noreply.github.com>
2021-11-21 21:24:37 +0200

    kelvin weather & while loop

---
+++ index.js
@@ -0,0 +1,21 @@
+// Kelvin Weather exercise 1
+const kelvin = 293;//kelvin will stay constant
+let celsius = kelvin - 273;//calculate celsius
+let fahrenheit = celsius * (9/5)+32;//Calculate Fahrenheit
+fahrenheit = Math.floor(fahrenheit);//Round down the Fahrenheit temperature
+console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`)
+
+// JS simple while loop
+let n = 1
+while (n <= 10 ) {
+  console.log( n )
+  n++
+}//interpreteaza o crestere de la 1 pina la 10 inclusiv si 10 ('<=') care sunt afisate intro consola
+
+// let n = 10
+// while (n >= 1 ) {
+//   console.log( n )
+//   n--
+// }
+
+
